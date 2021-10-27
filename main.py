3dfe0af1f49d9416f1ff9665bd20acfd61daf86d
alona77 <alonacojocarescu@gmail.com>
2021-10-27 11:10:38 +0300

    lab_567

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from tests.test_crud import test_create, test_read, test_update, test_delete
 from UserInterface.console import run_ui
 
 def main():
-    
     run_ui()
     
 if __name__ == '__main__':
