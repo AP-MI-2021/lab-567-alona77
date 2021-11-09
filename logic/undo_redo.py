d281258f5d0fdb94561ab8dc8b3a542e90766bdd
alona77 <alonacojocarescu@gmail.com>
2021-11-09 13:25:20 +0200

    pb6+7

--- logic/undo_redo.py
+++ logic/undo_redo.py
@@ -4,9 +4,9 @@ from typing import List
 def do_undo(undo_lst: list, redo_lst: list, curent_lst:list):
     """
     Se construieste fct de undo (anularea ultimei comenzi asupra listei)
-    :param undo_lst:
-    :param redo_lst:
-    :param curent_lst: 
+    :param undo_lst: lista de dinainte de vreo modificare
+    :param redo_lst:lista asupra careia a avut loc modificare
+    :param curent_lst: lista de rezervari in cazul acestei pb
     :return: lista asupra careia a actionat undo
     """
     if undo_lst:
@@ -20,10 +20,10 @@ def do_undo(undo_lst: list, redo_lst: list, curent_lst:list):
 def do_redo(undo_lst: list, redo_lst : list, current_lst: list):
     """
     fct de redo (care se poate aplica doar asupra unui undo)
-    :param undo_lst:
-    :param redo_lst:
-    :param current_lst:
-    :return:
+    :param undo_lst: lista care retine lista inainte de modificare
+    :param redo_lst:lista care a suferit modificare
+    :param current_lst:lista de rezervari
+    :return:lista asupra careia a actionat comanda redo
     """
     if redo_lst:
         top_redo=redo_lst.pop()
